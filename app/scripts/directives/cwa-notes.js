7480a44138c5c55817df5e9311544338bfdeda8e
jeffalexander <jeffrey.alan.alexander@gmail.com>
2013-11-04 19:47:14 -0500

    Ben added notes horrah.

---
+++ app/scripts/directives/cwa-notes.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('cookingWithAngularApp')
+  .directive('cwaNotes', function () {
+    return {
+      templateUrl: '/app/views/directives/cwaNotes.html',
+      restrict: 'E',
+      scope: {
+        notes : "="
+        },
+    
+        controller: function($scope){
+            $scope.newNote = {};
+            
+            $scope.addNote = function(){
+                $scope.notes.push($scope.newNote);
+                $scope.newNote = {};
+            }
+        }
+    }
+  });
