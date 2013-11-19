44187860ad5e7bb1e97edaeecca6f1559a49d2b9
Matt Senn <eternalmatt@gmail.com>
2013-11-18 23:45:20 -0500

    Directory structure cleanup

--- app/modules/recipe/steps/cwa-steps.js
+++ app/modules/recipe/steps/cwa-steps.js
@@ -3,7 +3,7 @@
 angular.module('cookingWithAngularApp')
   .directive('cwaSteps', function () {
     return {
-      templateUrl: '/app/recipe/steps/cwaSteps.html',
+      templateUrl: '/app/modules/recipe/steps/cwaSteps.html',
       restrict: 'E',
       scope: {
         steps : "="
