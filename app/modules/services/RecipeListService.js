df0b319f3af35000d88854eac33d00df4730b8ed
jeffalexander <jeffrey.alan.alexander@gmail.com>
2013-11-18 20:10:16 -0500

    service now pulls from all.json test file

--- app/modules/services/RecipeListService.js
+++ app/modules/services/RecipeListService.js
@@ -1,39 +1,9 @@
 'use strict';
 
 angular.module('cookingWithAngularApp')
-  .service('RecipeListService', function RecipeListService() {
-      this.dashboardRecipes = [
-            {
-                name: "Mac and cheese",
-                imageUrl: "http://du.cx/BrC",
-                servings: "3 men",
-                rating: "3",
-                ingredients: [
-                    {
-                        quantity: "3",
-                        unit: "cups",
-                        description: "milk"
-                    },
-                    {
-                        quantity: "1",
-                        unit: "tsp",
-                        description: "vanilla"
-                    },
-                    {
-                        quantity: "3",
-                        unit: "tbsp",
-                        description: "sugar"
-                    }
-                ],
-                steps: [
-                    {description: "get glass"},
-                    {description: "pour milk"},
-                    {description: "add vanilla and sugar"},
-                    {description: "mix"},
-                    {description: "drink"}
-                ]
-            }
-      ];
+  .service('RecipeListService', function RecipeListService($resource) {
+      var Recipes = $resource('/test/rest/recipes/all.json');
+      this.dashboardRecipes = Recipes.query();
       
       this.add = function(recipe) {
         this.dashboardRecipes.push(recipe);
