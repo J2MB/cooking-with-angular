0a9abb0c82090fc223d6a7aee08bff5eb29f3f9f
Matt Senn <eternalmatt@gmail.com>
2013-11-20 23:12:52 -0500

    Refactored the routes (again). Haters gonna hate.

--- app/modules/app.js
+++ app/modules/app.js
@@ -5,72 +5,77 @@ angular.module('cookingWithAngularApp', [
   'ngResource',
   'ngRoute',
   'ngSanitize',
-    'ngRoute',
+  'ngRoute',
   'ui.bootstrap'
 ])
-  .config(function ($routeProvider) {
-      
-    var recipeResolver = function(RecipeListService, $route, $q){
+.config(function($routeProvider) {
+    
+    //returns a promise that resolves to a list of recipes
+    var recipeListResolver = function(RecipeListService, $q) {
+        var deferred = $q.defer();
+        RecipeListService.getDashboardRecipes(deferred.resolve);
+        return deferred.promise;
+    };
+
+    //returns a promise that resolves to a recipe for a given id in the route
+    var recipeResolver = function(RecipeListService, $route, $q) {
         var id = $route.current.params.id;
         var deferred = $q.defer();
-        
-        var callback = function(recipe){
+
+        var callback = function(recipe) {
             if (recipe === undefined) {//more checking needed
                 deferred.reject("No recipe for this id: " + id);
             } else {
-                deferred.resolve(recipe);   
+                deferred.resolve(recipe);
             }
         };
-        
+
         RecipeListService.getDashboardRecipe(id, callback);
 
         return deferred.promise;
     };
-      
-    $routeProvider
-      .when('/', {
-        templateUrl: 'modules/dashboard/dashboard.html',
-        controller: 'DashboardController',
-        resolve: {
-            recipes: function(RecipeListService, $q){
-                var deferred = $q.defer();
-                var callback = deferred.resolve;
-                RecipeListService.getDashboardRecipes(callback);
-                return deferred.promise;
-            }
+    
+    //returns a promise that resolves to a blank recipe 
+    var blankRecipeResolver = function($q, RecipeListService){
+        return $q.when(RecipeListService.blankRecipe());    
+    };   
+
+    var flatResolver = function(value){
+        //returns a promise that resolves to the value 'mode'
+        return function($q){ return $q.when(value); };
+    };
+
+    $routeProvider.when('/', {
+        templateUrl : 'modules/dashboard/dashboard.html',
+        controller : 'DashboardController',
+        resolve : {
+            recipes : recipeListResolver
         }
-      })
-      .when('/recipe', {
-        templateUrl: 'modules/recipe/recipe.html',
-        controller: 'RecipeController',
-        resolve: {
-            newMode:  function($q){ return $q.when(true);  },
-            editMode: function($q){ return $q.when(false); },
-            recipe: function(RecipeListService, $q){
-                //returns a promise that resolves to a blank recipe.
-                return $q.when(RecipeListService.blankRecipe());
-            }
+    }).when('/recipe', {
+        templateUrl : 'modules/recipe/recipe.html',
+        controller : 'RecipeController',
+        resolve : {
+            recipe : blankRecipeResolver,
+            newMode: flatResolver(true),
+            editMode: flatResolver(false)
         }
-      })
-      .when('/recipe/:id', {
-        templateUrl: 'modules/recipe/recipe.html',
-        controller: 'RecipeController',
-        resolve: {
-            newMode:  function($q){ return $q.when(false);  },
-            editMode: function($q){ return $q.when(false); },
-            recipe: recipeResolver
+    }).when('/recipe/:id', {
+        templateUrl : 'modules/recipe/recipe.html',
+        controller : 'RecipeController',
+        resolve : {
+            recipe : recipeResolver,
+            newMode: flatResolver(false),
+            editMode: flatResolver(false)
         }
-      })
-      .when('/recipe/:id/edit', {
-        templateUrl: 'modules/recipe/recipe.html',
-        controller: 'RecipeController',
-        resolve: {
-            newMode:  function($q){ return $q.when(false);  },
-            editMode: function($q){ return $q.when(true); },
-            recipe: recipeResolver
+    }).when('/recipe/:id/edit', {
+        templateUrl : 'modules/recipe/recipe.html',
+        controller : 'RecipeController',
+        resolve : {
+            recipe : recipeResolver,
+            newMode: flatResolver(false),
+            editMode: flatResolver(true)
         }
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+    }).otherwise({
+        redirectTo : '/'
+    });
 });
