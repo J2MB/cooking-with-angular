d16336a5b7c6e38a2d90d4003b5e5daec028a67c
Matt Senn <eternalmatt@gmail.com>
2013-11-20 22:42:49 -0500

    Further whitespace refactor of code

--- app/modules/edit-on-click/edit-on-click.js
+++ app/modules/edit-on-click/edit-on-click.js
@@ -1,57 +1,48 @@
-new function(){
-var make = angular.element;
+angular.module("editOnClick", [])
+.directive('editOnClick', function editOnClickDirective() {
+    var make = angular.element;
+    return {
+        restrict : "A",
+        replace : false,
+        scope : {
+            editOnClick : '='
+        },
+        controller : function editOnClickController($scope) {
+            $scope.editText = $scope.editOnClick;
+        },
+        compile : function editOnClickCompile(tElement, tAttrs, transclude) {
+            var tType = tElement[0].tagName.toLowerCase();
+            var tDisplayStyle = getComputedStyle(tElement[0], null).display;
+            var myElem = make("<span editBlock class='noEdit'></span>");
+            var input = make("<input type='text' ng-model='editText' editInput style='display:" + tDisplayStyle + "'>");
+            var displayElem = make("<" + tType + " editContent>{{editOnClick}}</" + tType + ">");
+            myElem.append(input);
+            myElem.append(displayElem);
+            tElement.replaceWith(myElem);
 
-var edit = angular.module("editOnClick", []);
-edit.directive('editOnClick', function editOnClickDirective(){
-return {
+            //link
+            return function(scope, element, attrs) {
+                var input = make(element.children()[0]);
+                var displayElem = make(element.children()[1]);
 
-//Begin directive
-
-restrict: "A",
-replace: false,
-scope: {
-    editOnClick:'='
-},
-controller: function editOnClickController($scope)
-{$scope.editText = $scope.editOnClick;},
-
-compile: function editOnClickCompile(tElement, tAttrs, transclude)
-{
-    var tType = tElement[0].tagName.toLowerCase();
-    var tDisplayStyle = getComputedStyle(tElement[0], null).display;
-    var myElem = make("<span editBlock class='noEdit'></span>");
-    var input = make("<input type='text' ng-model='editText' editInput style='display:"+tDisplayStyle+"'>");
-    var displayElem = make("<"+tType+" editContent>{{editOnClick}}</"+tType+">");
-    myElem.append(input);
-    myElem.append(displayElem);
-    tElement.replaceWith(myElem);
-
-    //link
-    return function(scope, element, attrs)
-    {
-        var input = make(element.children()[0]);
-        var displayElem = make(element.children()[1]);
-
-        displayElem.on('click', function editOnClickDisplayElemClicked(event){
-            element.toggleClass("doEdit").toggleClass("noEdit");
-            scope.editText = scope.editOnClick;
-            scope.$apply();
-            input[0].focus();
-        });
-        input.on('blur',function editOnClickInputBlurred(event){
-            element.toggleClass("doEdit").toggleClass("noEdit");
-            scope.editOnClick = scope.editText;
-            scope.$apply();
-        });
-        input.on('keypress',function editOnClickInputKeyPressed(event){
-            if(event.keyCode == 13){this.blur();}
-        });
+                displayElem.on('click', function editOnClickDisplayElemClicked(event) {
+                    element.toggleClass("doEdit").toggleClass("noEdit");
+                    scope.editText = scope.editOnClick;
+                    scope.$apply();
+                    input[0].focus();
+                });
+                input.on('blur', function editOnClickInputBlurred(event) {
+                    element.toggleClass("doEdit").toggleClass("noEdit");
+                    scope.editOnClick = scope.editText;
+                    scope.$apply();
+                });
+                input.on('keypress', function editOnClickInputKeyPressed(event) {
+                    if (event.keyCode == 13) {
+                        this.blur();
+                    }
+                });
+            };
+        }
     };
-}
-
-//End directive
-
-}
 });
 
-}();
