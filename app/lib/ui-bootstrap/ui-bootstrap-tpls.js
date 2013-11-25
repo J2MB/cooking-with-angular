fb9f3f86376fd6387d01db69ac4caf8b9f428cf3
eternalmatt <eternalmatt@gmail.com>
2013-11-25 18:54:24 -0500

    Fixing dumb ui-bootstrap bug until they fix it.

--- app/lib/ui-bootstrap/ui-bootstrap-tpls.js
+++ app/lib/ui-bootstrap/ui-bootstrap-tpls.js
@@ -3470,7 +3470,7 @@ angular.module("template/progressbar/progress.html", []).run(["$templateCache", 
 angular.module("template/rating/rating.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("template/rating/rating.html",
     "<span ng-mouseleave=\"reset()\">\n" +
-    "	<i ng-repeat=\"r in range\" ng-mouseenter=\"enter($index + 1)\" ng-click=\"rate($index + 1)\" ng-class=\"$index < val && (r.stateOn || 'icon-star') || (r.stateOff || 'icon-star-empty')\"></i>\n" +
+    "	<i ng-repeat=\"r in range track by $index\" ng-mouseenter=\"enter($index + 1)\" ng-click=\"rate($index + 1)\" ng-class=\"$index < val && (r.stateOn || 'icon-star') || (r.stateOff || 'icon-star-empty')\"></i>\n" +
     "</span>");
 }]);
 
