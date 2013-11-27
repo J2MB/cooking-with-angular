2b2717a46d330155c6086bf6d3503d8323fae911
eternalmatt <eternalmatt@gmail.com>
2013-11-26 23:14:59 -0500

    Simplified RecipeListService

--- app/modules/services/RecipeListService.js
+++ app/modules/services/RecipeListService.js
@@ -1,25 +1,22 @@
 'use strict';
 
 angular.module('cookingWithAngularApp')
-  .service('RecipeListService', function RecipeListService($resource, $q) {
-      var Recipes = $resource('/app/rest/recipes/all.json');
-      
+  .service('RecipeListService', function RecipeListService($resource) {
+      var Recipes = $resource('rest/recipes/all.json');
       var request = Recipes.query();
-      this.promise = request.$promise;
+      var promise = request.$promise;
       
       this.addRecipe = function(recipe) { //this is kinda dumb now. but it works!
-          this.promise.then(function(data){
+          promise.then(function(data){
             data.push(recipe);
             return data;
           });
       };
       
-      this.getDashboardRecipes = function(callback){
-          this.promise.then(callback);
-      };
+      this.getDashboardRecipes = promise.then; // lol, too simple.
       
       this.getDashboardRecipe = function(id, callback) {
-         this.promise.then(function(recipes){
+         promise.then(function(recipes){
             var recipe = recipes[id];
             callback(recipe);
          });
