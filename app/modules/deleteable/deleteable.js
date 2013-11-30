ea381e96f5b8c90241d428ce25ebf5cb05ad81de
eternalmatt <eternalmatt@gmail.com>
2013-11-30 12:37:25 -0500

    Deleteable directive now transcludes template

--- app/modules/deleteable/deleteable.js
+++ app/modules/deleteable/deleteable.js
@@ -1,30 +1,34 @@
 'use strict';
 
 angular.module('cookingWithAngularApp')
-.directive('deleteable', function() {
+.directive('deleteable', function($animate) {
     return {
-        restrict : 'E',
-        replace : true,
-        templateUrl : 'modules/deleteable/deleteable.html',
-        scope : {
-            index : '=',
-            list : '='
+        restrict: 'EA',
+        transclude: true,
+        templateUrl: 'modules/deleteable/deleteable.html',
+        scope: {
+            list: '=',
+            index: '='
         },
-        controller : function($scope) {
-            $scope.remove = function(index) {
-                $scope.list.splice(index, 1);
+        link: function preLink($scope, element, attrs, controller){ // no D.I. here.
+            
+            var list = $scope.list;
+            var remove = function(index) {
+                list.splice(index, 1);
             };
+            $scope.remove = remove;
 
             $scope.moveUp = function(index) {
-                var temp = $scope.list[index];
-                $scope.remove(index);
-                $scope.list.splice(index - 1, 0, temp);
+                var temp = list[index];
+                remove(index);
+                list.splice(index - 1, 0, temp);
+                //$animate.move(element, undefined, undefined);
             };
 
             $scope.moveDown = function(index) {
-                var temp = $scope.list[index];
-                $scope.remove(index);
-                $scope.list.splice(index + 1, 0, temp);
+                var temp = list[index];
+                remove(index);
+                list.splice(index + 1, 0, temp);
             };
         }
     };
